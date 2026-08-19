b89e2796c0e8ebaf0e105b38aae74585c9a4ce06
HESBOYALNE <4gs6776@gmail.com>
2026-08-19 01:16:55 +0100

    Add service worker for asset caching
    Implement a service worker for caching assets.

---
+++ sw.js
@@ -0,0 +1,23 @@
+// Brixo-TechFX Service Worker
+const CACHE_NAME = 'brixo-techfx-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './bfx-logo.jpeg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
